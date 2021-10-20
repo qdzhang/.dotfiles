8ee51d70f8828f871d092f8ea3f2cd37a6a124d5
qdzhang <qdzhangcn@gmail.com>
2021-10-20 15:50:37 +0800

    Add system config files in /etc directory

---
+++ system.conf/README.md
@@ -0,0 +1,1 @@
+System config files in `/etc` directory
---
+++ system.conf/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://www.laggner.at/config-file-backup-with-git/
+
+find . -maxdepth 1 -mindepth 1 ! -regex "./\(config_files.txt\|copy.sh\)" | xargs rm –r
+cat config_files.txt | xargs -I {} rsync -rR {} .
